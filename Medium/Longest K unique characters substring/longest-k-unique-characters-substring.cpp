6285ccbc6e2c152d576ed526f8c99802966ef8cf
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-26 21:29:43 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest K unique characters substring/longest-k-unique-characters-substring.cpp
+++ Medium/Longest K unique characters substring/longest-k-unique-characters-substring.cpp
@@ -10,47 +10,16 @@ using namespace std;
 class Solution{
   public:
     int longestKSubstr(string s, int k) {
-        // your code here
-        unordered_map<char,int> m;
-        int ans =0;
-        int i=0,j=0;
-        while(j < s.length()){
-
-            // if curr char is not in the map so we need to remove one char type
-            if(m.find(s[j]) == m.end() && m.size() == k){
-                // Store the current length in ans 
-                ans = max(ans,j-i);
-                
-                // until the map size is k remove char from front of window
-                while(m.size() == k){
-                    
-                    // removing the freq of char at front
-                    m[s[i]]--;
-                    
-                    // if the freq of char at front became zero then the k will be reduced to k -1
-                    // now we can include our curr char in the window
-                    if(m[s[i]] == 0){
-                        m.erase(s[i]);
-                        m[s[j]]++;
-                        i++;
-                        // cout<<i<<"\n";
-                        break;
-                    }
-                    i++;
-                }
-            }else{
-                m[s[j]]++;
-            }
-            j++;
+    // your code here
+        int cnt[26] = {}, distinct = 0, i = 0, j = 0, ans = 0, N = s.size();
+        while (j < N) {
+            distinct += cnt[s[j++]-'a']++ == 0;
+            while (distinct > k) distinct -= --cnt[s[i++]-'a'] == 0;
+            ans = max(ans, j - i);
         }
-        
-        // cout << m.size()<<"\n";
-        if(m.size() != k)return -1;
-        ans = max(ans,j-i);
-        return ans;
-        
+        if(distinct == k)return ans;
+        return -1;
     }
-    
 };
 
 //{ Driver Code Starts.
