45e02191db700fe9cb3e704f25b8bded9a78f12f
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-29 19:15:49 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number Of Enclaves/number-of-enclaves.cpp
+++ Medium/Number Of Enclaves/number-of-enclaves.cpp
@@ -10,48 +10,47 @@ using namespace std;
 
 class Solution {
   public:
-  void dfs(int r,int c,int n,int m,vector<vector<int>> &grid,vector<vector<int>> &vis){
-      vis[r][c] = 1;
-      vector<int> dir = {-1,0,1,0,-1};
-      
-      for(int i= 0; i< 4; i++){
-          int row = r + dir[i];
-          int col = c + dir[i+1];
-          
-          if(row >= 0 && row < n && col >=0 && col < m && grid[row][col] == 1 &&  vis[row][col] == 0){
-              dfs(row,col,n,m,grid,vis);
-          }
-      }
-  }
-    int numberOfEnclaves(vector<vector<int>> &grid) {
-        // Code here
-        int  n= grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
+    void dfs(int i,int j, vector<vector<int>>& grid,vector<vector<bool>>& vis){
+        vis[i][j] = true;
         
-        for(int i=0;i < n;i++){
-            if(grid[i][0] == 1){
-                dfs(i,0,n,m,grid,vis);
+        vector<int> dir = {-1,0,1,0,-1};
+        
+        for(int k=0; k < 4; k++){
+            int x = i + dir[k];
+            int y = j + dir[k+1];
+            
+            if(x >=0 && y >=0 && x < grid.size() && y < grid[0].size() && grid[x][y] == 1 && !vis[x][y]){
+                dfs(x,y,grid,vis);
             }
             
+        }
+    }
+    int numberOfEnclaves(vector<vector<int>> &grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        
+        for(int i=0; i< n; i++){
+            if(grid[i][0] == 1){
+                dfs(i,0,grid,vis);
+            }
             if(grid[i][m-1] == 1){
-                dfs(i,m-1,n,m,grid,vis);
+                dfs(i,m-1,grid,vis);
             }
         }
         
-        for(int j=0;j < m;j++){
+        for(int j= 0; j < m; j++){
             if(grid[0][j] == 1){
-                dfs(0,j,n,m,grid,vis);
+                dfs(0,j,grid,vis);
             }
-            
             if(grid[n-1][j] == 1){
-                dfs(n-1,j,n,m,grid,vis);
+                dfs(n-1,j,grid,vis);
             }
         }
         int cnt = 0;
-        for(int i=0;i < n; i++){
-            for(int j=0; j <m; j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0){
+        for(int i= 1; i< n-1; i++){
+            for(int j= 1; j< m-1;j++){
+                if(vis[i][j] != true && grid[i][j] == 1){
                     cnt++;
                 }
             }
