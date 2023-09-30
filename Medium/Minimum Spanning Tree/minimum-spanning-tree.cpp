1e9644daa195e4d5aea1aeb015b3c031958beb0d
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-30 21:49:31 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -3,6 +3,60 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class Disjointset {
+	vector<int> rank,parent,size;
+public:
+		Disjointset(int n){
+			rank.resize(n+1, 0);
+			parent.resize(n+1);
+			size.resize(n+1);
+
+			for(int i=0; i <= n; i++){
+				parent[i] = i;
+				size[i] = 1;
+			}
+		}
+
+		int findParent(int node){
+			if(parent[node] == node){
+				return node;
+			}
+			return parent[node] = findParent(parent[node]);
+		}
+
+		void unionByRank(int u, int v){
+			int ulp_u = findParent(u);
+			int ulp_v = findParent(v);
+			if(ulp_u == ulp_v)return;
+			if(rank[ulp_u] < rank[ulp_v]){
+				parent[ulp_u] = ulp_v;
+			}
+			else if(rank[ulp_v] < rank[ulp_u]){
+				parent[ulp_v] = ulp_u;
+			}
+			else{
+				parent[ulp_v] = ulp_u;
+				rank[ulp_u]++;
+			}
+		}
+
+		void unionBySize(int u,int v){
+			int ulp_u = findParent(u);
+			int ulp_v = findParent(v);
+
+			if(ulp_u == ulp_v)return;
+
+			if(size[ulp_v] < size[ulp_u]){
+				parent[ulp_v] = ulp_u;
+				size[ulp_u] += size[ulp_v];
+			}else{
+				parent[ulp_u] = ulp_v;
+				size[ulp_v] += ulp_u;
+			}
+		}
+};
+
 class Solution
 {
 	public:
@@ -10,34 +64,70 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> vis(V,0);
+       // PRIM'S ALGORITHM
+       
+        // priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        // vector<int> vis(V,0);
         
-        pq.push({0,0});
-        int sum = 0;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
+        // pq.push({0,V-1});
+        // int sum = 0;
+        // while(!pq.empty()){
+        //     auto it = pq.top();
+        //     pq.pop();
             
-            int node = it.second;
-            if(vis[node] == 1)continue;
+        //     int node = it.second;
+        //     if(vis[node] == 1)continue;
             
-            vis[node] = 1;
-            int wt = it.first;
+        //     vis[node] = 1;
+        //     int wt = it.first;
             
-            sum += wt;
+        //     sum += wt;
             
-            for(auto itr: adj[node]){
-                int adjNode = itr[0];
-                int edWt = itr[1];
+        //     for(auto itr: adj[node]){
+        //         int adjNode = itr[0];
+        //         int edWt = itr[1];
                 
-                if(!vis[adjNode]){
-                    pq.push({edWt,adjNode});
-                }
+        //         if(!vis[adjNode]){
+        //             pq.push({edWt,adjNode});
+        //         }
+        //     }
+        // }
+        
+        // return sum;
+        
+        
+        // KRUSKAL'S ALGRORITHM
+        
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0; i< V; i++){
+            for(auto it: adj[i]){
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+                
+                edges.push_back({wt,{node,adjNode}});
+            }    
+        }
+        
+        Disjointset ds(V);
+        
+        sort(edges.begin(),edges.end());
+        
+        int mstWt = 0;
+        
+        for(auto it : edges){
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findParent(u) != ds.findParent(v)){
+                mstWt += wt;
+                ds.unionBySize(u,v);
             }
         }
         
-        return sum;
+        return mstWt;
+        
     }
 };
 
