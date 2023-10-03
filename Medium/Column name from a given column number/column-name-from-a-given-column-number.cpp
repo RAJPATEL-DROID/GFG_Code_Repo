1512d97947a39693e811ca4c629675639e9f2270
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-10-03 11:58:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Column name from a given column number/column-name-from-a-given-column-number.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string ans;
+        
+        while(n > 0){
+            
+            int rem= n%26;
+            n= n/26;
+            
+            if(rem == 0){
+                ans = 'Z' + ans; 
+                n = n-1;
+            }else{
+                ans = char(rem + 64) + ans;
+            }
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+
+// } Driver Code Ends
