029a47eb772eb28478a2d6bc570cd2bb9baf5041
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-10-05 11:39:47 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count number of substrings/count-number-of-substrings.cpp
+++ Medium/Count number of substrings/count-number-of-substrings.cpp
@@ -11,40 +11,31 @@ using namespace std;
 class Solution
 {
   public:
-  
-    long long int countDistinct(string s,int k){
-        int i=0;
-        int j= 0;
-        int dist_chr = 0;
-        int n = s.length();
-        int char_cnt[26] ={0};
-        long long int ans =0;
-        while(j < n){
-            char_cnt[s[j]-97]++;
-            
-            if(char_cnt[s[j]-97] == 1) dist_chr++;
-            
-            while(dist_chr > k){
-                char_cnt[s[i]-97]--;
-                
-                if(char_cnt[s[i] - 97] == 0){
-                    dist_chr--;
-                }
-                i++;
-            }
-            
-            ans += j+1-i;
-            j++;
-        }
-        
-        return ans;
-    }
-    
-    
+  long long int countAtmostK(string s,int k){
+      vector<long long int> freq(26,0);
+      long long int start =0, count = 0,distinct =0;
+      
+      for(int i=0;i <s.size(); i++){
+          if(freq[s[i]-'a'] == 0)distinct++;
+          freq[s[i]-'a']++;
+          
+          while(distinct > k){
+              freq[s[start]-'a']--;
+              if(freq[s[start] - 'a'] == 0){
+                  distinct--;
+              }
+              start++;
+          }
+          if(distinct <= k){
+              count += i - start+1;
+          }
+      }
+      
+      return count;
+  }
     long long int substrCount (string s, int k) {
-    	long long int ans = countDistinct(s,k) - countDistinct(s,k-1);
-    	return ans;
-    	  
+    	//code here.
+    	return countAtmostK(s,k) - countAtmostK(s,k-1);
     }
 };
 
