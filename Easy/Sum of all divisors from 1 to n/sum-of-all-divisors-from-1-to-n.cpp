ebea865bf9dc9db663a6ec161e2a0b064360cfe9
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-10-21 11:30:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of all divisors from 1 to n/sum-of-all-divisors-from-1-to-n.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        // Write Your Code here
+        long long sum =0,i=1;
+        while(i <= N){
+            int count = N/i;
+            sum = sum+ i*count;
+            i++;
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
