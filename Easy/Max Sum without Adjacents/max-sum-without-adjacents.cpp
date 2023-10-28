581aad94161051fc14c40ec6829ffd1ae4087ead
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-10-28 18:11:32 +0530

    Updated Solution - GfG to GitHub

--- Easy/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Easy/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -8,26 +8,24 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-    int solve(int *arr,int curr,vector<int>& dp){
-        if(curr == 0)return dp[0] = arr[0];
-        if(curr < 0)return 0;
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(vector<int>&arr, int n) {
+	    // code here
+	    int prev1 =arr[0];
+        int prev2 =0;
         
-        if(dp[curr] != -1){
-            return dp[curr];
+        for(int i=1;i< n; i++){
+            int left = arr[i];
+            if(i>1){
+                left += prev2;
+            }
+            int right = prev1;
+            int curr = max(left,right);
+            prev2 = prev1;
+            prev1 = curr;
         }
         
-        int left = solve(arr,curr-2,dp) + arr[curr];
-        
-        int right = solve(arr,curr-1,dp);
-        
-        return dp[curr] = max(left,right);
- 
-    }
-	// calculate the maximum sum with out adjacent
-	int findMaxSum(int *arr, int n) {
-	    // code here
-	    vector<int> dp(n,-1);
-	    return solve(arr,n-1,dp);
+        return prev1;
 	}
 };
 
@@ -39,7 +37,7 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        int arr[n];
+        vector<int>arr(n);
         for (int i = 0; i < n; i++) {
             cin >> arr[i];
         }
