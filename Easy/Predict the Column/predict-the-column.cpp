94906d4f91830cce477ff5ecbeb0851287c8ddb7
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-11-09 23:27:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Predict the Column/predict-the-column.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    /*Function to count zeros in each column
+    * N : Number of rows and columns in array
+    M is the matrix that is globally declared
+    */
+    int columnWithMaxZeros(vector<vector<int>>arr,int N){
+        
+        // Your code here
+        int ans=0,zero=0;
+        for(int j=0; j < N; j++){
+            int temp =0;
+            for(int i=0;i < N; i++){
+                if(arr[i][j] == 0)temp++;
+            }
+            if(temp > zero){
+                zero = temp;
+                ans = j;
+            }
+        }
+        
+        if(zero == 0)return -1;
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    vector<vector<int>>arr(N,vector<int>(N));
+	    // Taking elements input into the matrix
+	    for(int i = 0;i<N;i++){
+	        for(int j = 0;j<N;j++){
+	            int x;
+	            cin >> x; 
+	            arr[i][j]=x;
+	        }
+	    }
+	    Solution ob;
+	    cout << ob.columnWithMaxZeros(arr,N) << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
